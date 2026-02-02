bf533515bf2890622f7eeab07a4ab2b76c8bb5e9
Cannon Kalra <cannonkalra@gmail.com>
2026-02-02 12:36:31 +0530

    [Add] magic ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cannonkalra.github.io',
+  site: "https://cannonkalra.github.io",
+
+  integrations: [
+    react(),
+    // tailwind({
+    //   applyBaseStyles: false,
+    // }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
